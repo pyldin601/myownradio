f836d20104c71bfe56775a2d8179c14cdba98703
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2025-04-22 22:47:50 +0100

    Add `actix-web` and web server skeleton

--- services/rust/crates/audio-composer/src/main.rs
+++ services/rust/crates/audio-composer/src/main.rs
@@ -1,14 +1,28 @@
 use crate::config::Config;
+use actix_web::web::Data;
+use actix_web::{App, HttpServer};
 use scheduler_client::scheduler_client::SchedulerClient;
 
 mod config;
 
+const SHUTDOWN_TIMEOUT: u64 = 30;
+
+#[dotenvy::load]
 #[actix_rt::main]
 async fn main() -> Result<(), std::io::Error> {
     let config = Config::from_env();
     let scheduler_client = SchedulerClient::new(&config.scheduler_endpoint);
 
-    println!("Hello, world!");
+    let bind_address = config.bind_address.clone();
 
-    Ok(())
+    let server = HttpServer::new(move || {
+        App::new()
+            .app_data(Data::new(config.clone()))
+            .app_data(Data::new(scheduler_client.clone()))
+    })
+    .shutdown_timeout(SHUTDOWN_TIMEOUT)
+    .bind(bind_address)?
+    .run();
+
+    server.await
 }
