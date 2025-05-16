ca8114d9969eb6082325a79bf95942775c45d91c
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2025-05-16 20:51:06 +0100

    feat(mor-api-gateway): Introduce `Track` model

--- services/rust/crates/mor-api-gateway/src/db/mod.rs
+++ services/rust/crates/mor-api-gateway/src/db/mod.rs
@@ -1,5 +1,6 @@
 pub(crate) mod channels;
 mod pool;
 mod schema;
+pub(crate) mod tracks;
 
 pub(crate) use pool::DbPool;
---
+++ services/rust/crates/mor-api-gateway/src/db/tracks/model.rs
@@ -0,0 +1,36 @@
+use crate::db::schema::r_tracks;
+use diesel::{Queryable, Selectable};
+use serde::Serialize;
+
+#[derive(Clone, Serialize, Selectable, Queryable)]
+#[diesel(table_name = r_tracks)]
+#[diesel(check_for_backend(diesel::mysql::Mysql))]
+#[allow(dead_code)]
+pub(crate) struct Track {
+    #[diesel(column_name = "tid")]
+    pub(crate) id: i32,
+    #[diesel(column_name = "uid")]
+    pub(crate) user_id: i32,
+    pub(crate) file_id: Option<i32>,
+    pub(crate) filename: String,
+    pub(crate) hash: String,
+    pub(crate) ext: String,
+    pub(crate) artist: String,
+    pub(crate) title: String,
+    pub(crate) album: String,
+    pub(crate) track_number: String,
+    pub(crate) genre: String,
+    pub(crate) date: String,
+    pub(crate) cue: Option<String>,
+    pub(crate) buy: Option<String>,
+    pub(crate) duration: i32,
+    pub(crate) filesize: i32,
+    pub(crate) color: i32,
+    pub(crate) uploaded: i32,
+    pub(crate) copy_of: Option<i32>,
+    pub(crate) used_count: i32,
+    pub(crate) is_new: i8,
+    pub(crate) can_be_shared: i8,
+    pub(crate) is_deleted: i8,
+    pub(crate) deleted: Option<i32>,
+}
