c145e81cd97e7d324834ef3a288e1aed9fc6f575
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2025-05-31 15:58:28 +0100

    feat(mor-api-gateway): Introduce `UserId` in `web-user-identity` crate

--- services/rust/crates/mor-api-gateway/src/router/me/tracks.rs
+++ services/rust/crates/mor-api-gateway/src/router/me/tracks.rs
@@ -1,42 +1,43 @@
 use crate::response::Response;
 use actix_web::HttpResponse;
+use web_user_identity::UserId;
 
-pub(crate) async fn list_tracks() -> Response {
+pub(crate) async fn list_tracks(user_id: UserId) -> Response {
     Ok(HttpResponse::NotImplemented().finish())
 }
 
-pub(crate) async fn list_unused_tracks() -> Response {
+pub(crate) async fn list_unused_tracks(user_id: UserId) -> Response {
     Ok(HttpResponse::NotImplemented().finish())
 }
 
-pub(crate) async fn search_tracks() -> Response {
+pub(crate) async fn search_tracks(user_id: UserId) -> Response {
     Ok(HttpResponse::NotImplemented().finish())
 }
 
-pub(crate) async fn upload_track() -> Response {
+pub(crate) async fn upload_track(user_id: UserId) -> Response {
     Ok(HttpResponse::NotImplemented().finish())
 }
 
-pub(crate) async fn add_to_channel() -> Response {
+pub(crate) async fn add_to_channel(user_id: UserId) -> Response {
     Ok(HttpResponse::NotImplemented().finish())
 }
 
-pub(crate) async fn change_color() -> Response {
+pub(crate) async fn change_color(user_id: UserId) -> Response {
     Ok(HttpResponse::NotImplemented().finish())
 }
 
-pub(crate) async fn edit_multiple_tracks() -> Response {
+pub(crate) async fn edit_multiple_tracks(user_id: UserId) -> Response {
     Ok(HttpResponse::NotImplemented().finish())
 }
 
-pub(crate) async fn delete_multiple_tracks() -> Response {
+pub(crate) async fn delete_multiple_tracks(user_id: UserId) -> Response {
     Ok(HttpResponse::NotImplemented().finish())
 }
 
-pub(crate) async fn download() -> Response {
+pub(crate) async fn download(user_id: UserId) -> Response {
     Ok(HttpResponse::NotImplemented().finish())
 }
 
-pub(crate) async fn preview() -> Response {
+pub(crate) async fn preview(user_id: UserId) -> Response {
     Ok(HttpResponse::NotImplemented().finish())
 }
