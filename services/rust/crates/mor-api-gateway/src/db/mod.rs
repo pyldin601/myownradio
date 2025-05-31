c72db69f4e515fd775439f489c1ef10c987a4831
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2025-05-31 17:50:09 +0100

    feat(mor-api-gateway): Implement login using legacy session and cookie

--- services/rust/crates/mor-api-gateway/src/db/mod.rs
+++ services/rust/crates/mor-api-gateway/src/db/mod.rs
@@ -1,8 +1,10 @@
-pub(crate) mod channel_tracks;
-pub(crate) mod channels;
-mod files;
 mod pool;
 mod schema;
+
+pub(crate) mod channel_tracks;
+pub(crate) mod channels;
+pub(crate) mod files;
+pub(crate) mod sessions;
 pub(crate) mod tracks;
 pub(crate) mod users;
 
--- services/rust/crates/mor-api-gateway/src/router/auth/auth.rs
+++ services/rust/crates/mor-api-gateway/src/router/auth/auth.rs
@@ -1,8 +1,16 @@
 use crate::auth;
-use crate::db::{users, DbPool};
+use crate::auth::legacy;
+use crate::config::Config;
+use crate::db::{sessions, users, DbPool};
 use crate::response::Response;
+use actix_web::cookie::time::OffsetDateTime;
+use actix_web::cookie::CookieBuilder;
 use actix_web::{web, HttpResponse};
+use chrono::Utc;
 use tracing::error;
+
+const LEGACY_SESSION_COOKIE_NAME: &str = "secure_session";
+const YEAR: std::time::Duration = std::time::Duration::from_secs(31_536_000);
 
 #[derive(serde::Deserialize)]
 pub(crate) struct LoginRequestBody {
@@ -20,7 +28,11 @@ pub(crate) struct LoginRequestBody {
 /// - `200 OK` on success
 /// - `401 Unauthorized` if credentials are invalid
 /// - `501 Not Implemented` (placeholder for now)
-pub(crate) async fn login(body: web::Json<LoginRequestBody>, pool: web::Data<DbPool>) -> Response {
+pub(crate) async fn login(
+    body: web::Json<LoginRequestBody>,
+    pool: web::Data<DbPool>,
+    config: web::Data<Config>,
+) -> Response {
     let LoginRequestBody { email, password } = body.into_inner();
 
     let mut conn = pool.get_connection().await?;
@@ -47,9 +59,35 @@ pub(crate) async fn login(body: web::Json<LoginRequestBody>, pool: web::Data<DbP
         return Ok(HttpResponse::Unauthorized().finish());
     }
 
-    // TODO Create session
+    // Create a legacy session
+    let token = uuid::Uuid::new_v4().to_string().replace("-", "");
+    let session_id = uuid::Uuid::new_v4().to_string();
+    let session = sessions::Session {
+        token,
+        session_id,
+        uid: user.uid,
+        ip: String::default(),
+        client_id: String::default(),
+        authorized: Utc::now().naive_utc(),
+        permanent: 1,
+        expires: None,
+        http_user_agent: String::default(),
+    };
+    sessions::create(&session, &mut conn).await?;
 
-    Ok(HttpResponse::NotImplemented().finish())
+    let claims = legacy::LegacyAuthTokenClaims {
+        id: session.session_id,
+        data: legacy::LegacyAuthTokenData {
+            token: session.token,
+        },
+    };
+    let signature = legacy::sign_legacy_claims(&claims, &config.auth_legacy_session_secret_key);
+
+    let session_cookie = CookieBuilder::new(LEGACY_SESSION_COOKIE_NAME, signature)
+        .expires(OffsetDateTime::now_utc() + YEAR)
+        .finish();
+
+    Ok(HttpResponse::Ok().cookie(session_cookie).json(user))
 }
 
 #[derive(serde::Deserialize)]
