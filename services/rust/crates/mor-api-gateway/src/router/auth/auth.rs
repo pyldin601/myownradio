2f76e5cfdc6db951ef8bc77b6f6939ea76b615ba
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2025-05-31 15:36:07 +0100

    feat(mor-api-gateway): Introduce a request body for auth route handlers

--- services/rust/crates/mor-api-gateway/src/router/auth/auth.rs
+++ services/rust/crates/mor-api-gateway/src/router/auth/auth.rs
@@ -1,5 +1,11 @@
 use crate::response::Response;
-use actix_web::HttpResponse;
+use actix_web::{web, HttpResponse};
+
+#[derive(serde::Deserialize)]
+pub(crate) struct LoginRequestBody {
+    email: String,
+    password: String,
+}
 
 /// Logs a user in using email and password credentials.
 ///
@@ -11,8 +17,15 @@ use actix_web::HttpResponse;
 /// - `200 OK` on success
 /// - `401 Unauthorized` if credentials are invalid
 /// - `501 Not Implemented` (placeholder for now)
-pub(crate) async fn login() -> Response {
+pub(crate) async fn login(body: web::Json<LoginRequestBody>) -> Response {
+    let LoginRequestBody { email, password } = body.into_inner();
+
     Ok(HttpResponse::NotImplemented().finish())
+}
+
+#[derive(serde::Deserialize)]
+pub(crate) struct ResetPasswordRequestBody {
+    email: String,
 }
 
 /// Initiates a password reset flow for the given email address.
@@ -24,8 +37,15 @@ pub(crate) async fn login() -> Response {
 /// - `200 OK` regardless of email existence. If the email is associated with an account, an email with instructions is sent
 /// - `400 Bad Request` if the email is invalid
 /// - `501 Not Implemented` (placeholder for now)
-pub(crate) async fn reset_password() -> Response {
+pub(crate) async fn reset_password(body: web::Json<ResetPasswordRequestBody>) -> Response {
+    let ResetPasswordRequestBody { email } = body.into_inner();
+
     Ok(HttpResponse::NotImplemented().finish())
+}
+
+#[derive(serde::Deserialize)]
+pub(crate) struct SignupRequestBody {
+    email: String,
 }
 
 /// Sends an email with signup instructions. If the email is associated with an account, it should send an email with login or password reset instructions.
@@ -36,7 +56,9 @@ pub(crate) async fn reset_password() -> Response {
 /// Returns:
 /// - `200 OK` email has been sent
 /// - `501 Not Implemented` (placeholder for now)
-pub(crate) async fn signup() -> Response {
+pub(crate) async fn signup(body: web::Json<SignupRequestBody>) -> Response {
+    let SignupRequestBody { email } = body.into_inner();
+
     // TODO: if the email is associated with an account, send login instructions
     // TODO: otherwise, send an email explaining that signup is currently disabled
     Ok(HttpResponse::NotImplemented().finish())
