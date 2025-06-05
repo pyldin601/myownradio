2b416e57749807f4aefd0c32172c4c94e899101c
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2025-06-05 20:11:13 +0100

    feat(mor-api-gateway): Implement delete_expired() query function

--- services/rust/crates/mor-api-gateway/src/db/sessions/queries.rs
+++ services/rust/crates/mor-api-gateway/src/db/sessions/queries.rs
@@ -1,6 +1,7 @@
 use crate::db::schema::r_sessions::dsl as columns;
 use crate::db::schema::r_sessions::dsl::r_sessions;
 use crate::db::sessions::model::Session;
+use chrono::Utc;
 use diesel::prelude::*;
 use diesel::result::Error;
 use diesel_async::{AsyncMysqlConnection, RunQueryDsl};
@@ -20,6 +21,18 @@ pub(crate) async fn create(
 pub(crate) async fn delete(token: &str, conn: &mut AsyncMysqlConnection) -> Result<(), Error> {
     diesel::delete(r_sessions)
         .filter(columns::token.eq(token))
+        .execute(conn)
+        .await?;
+
+    Ok(())
+}
+
+pub(crate) async fn delete_expired(conn: &mut AsyncMysqlConnection) -> Result<(), Error> {
+    let now = Utc::now().naive_utc();
+
+    // TODO Optimize query by indexing expires column
+    diesel::delete(r_sessions)
+        .filter(columns::expires.le(now))
         .execute(conn)
         .await?;
 
